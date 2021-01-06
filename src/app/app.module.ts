8b4f0b4b16c31cbc81a820e28313174b8907654f
Aleksander Bogaczyk <aleksander.bogaczyk@outlook.com>
2021-01-06 21:00:51 +0100

    Dodane etykiety

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokemonListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
